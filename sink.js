835b9087793a7ca6de94bdaf2cdb89bf6f7e919f
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-01-04 15:25:58 +0200

    Callback Proxy API

--- sink.js
+++ sink.js
@@ -316,6 +316,21 @@ SinkClass.prototype = {
 		}
 	},
 /**
+ * Creates a proxy callback system for the sink instance.
+ *
+ * @method Sink
+ *
+ * @arg {Number} !bufferSize The buffer size for the proxy.
+*/
+	createProxy: function (bufferSize) {
+		var	proxy		= new Sink.Proxy(bufferSize, this.channelCount);
+		proxy.parentSink	= this;
+
+		this.on('audioprocess', proxy.callback);
+
+		return proxy;
+	},
+/**
  * Private method that handles the mixing of asynchronously written buffers.
  *
  * @method Sink
@@ -856,6 +871,51 @@ Sink.singleton = function () {
 
 	return sink;
 };
+
+function Proxy (bufferSize, channelCount) {
+	Sink.EventEmitter.call(this);
+
+	this.bufferSize		= isNaN(bufferSize) || bufferSize === null ? this.bufferSize : bufferSize;
+	this.channelCount	= isNaN(channelCount) || channelCount === null ? this.channelCount : channelCount;
+
+	var self = this;
+	this.callback	= function () {
+		return self.process.apply(self, arguments);
+	};
+
+	this.resetBuffer();
+}
+
+Proxy.prototype = {
+	buffer: null,
+	zeroBuffer: null,
+	parentSink: null,
+	bufferSize: 4096,
+	channelCount: 2,
+	offset: null,
+
+	resetBuffer: function () {
+		this.buffer	= new Float32Array(this.bufferSize);
+		this.zeroBuffer	= new Float32Array(this.bufferSize);
+	},
+
+	process: function (buffer, channelCount) {
+		this.offset === null && this.loadBuffer();
+
+		for (var i=0; i<buffer.length; i++) {
+			this.offset >= this.buffer.length && this.loadBuffer();
+			buffer[i] = this.buffer[this.offset++];
+		}
+	},
+
+	loadBuffer: function () {
+		this.offset = 0;
+		Sink.memcpy(this.zeroBuffer, 0, this.buffer, 0);
+		this.emit('audioprocess', [this.buffer, this.channelCount]);
+	},
+};
+
+Sink.Proxy = Proxy;
 
 (function(){
 
