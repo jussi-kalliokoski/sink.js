160920c1322a679a9151085d078f8940f8b25ece
Jussi Kalliokoski <jussi.kalliokoski@gmail.com>
2012-06-16 13:58:51 +0300

    Rewrote inlineWorker to use createDynURL
     * Now supports Blobs (alongside with deprecated BlobBuilders).
     * Defined properties of the created workers instead of assigning to
       them to be less intrusive.

--- src/core/inline-worker.js
+++ src/core/inline-worker.js
@@ -1,9 +1,5 @@
 void function (Sink) {
 
-var	BlobBuilder	= typeof window === 'undefined' ? undefined :
-	window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder || window.OBlobBuilder,
-	URL		= typeof window === 'undefined' ? undefined : (window.URL || window.MozURL || window.webkitURL || window.MSURL || window.OURL);
-
 /**
  * Creates an inline worker using a data/blob URL, if possible.
  *
@@ -14,54 +10,80 @@ var	BlobBuilder	= typeof window === 'undefined' ? undefined :
  * @return {Worker} A web worker, or null if impossible to create.
 */
 
+var define = Object.defineProperty ? function (obj, name, value) {
+	Object.defineProperty(obj, name, {
+		value: value,
+		configurable: true,
+		writable: true
+	});
+} : function (obj, name, value) {
+	obj[name] = value;
+};
+
+function terminate () {
+	define(this, 'terminate', this._terminate);
+
+	Sink.revokeDynURL(this._url);
+
+	delete this._url;
+	delete this._terminate;
+	return this.terminate();
+}
+
 function inlineWorker (script) {
-	var	worker	= null,
-		url, bb;
-	try {
-		bb	= new BlobBuilder();
-		bb.append(script);
-		url	= URL.createObjectURL(bb.getBlob());
-		worker	= new Worker(url);
+	function wrap (type, content, typeName) {
+		try {
+			var url = type(content, 'text/javascript');
+			var worker = new Worker(url);
 
-		worker._terminate	= worker.terminate;
-		worker._url		= url;
-		bb			= null;
+			define(worker, '_url', url);
+			define(worker, '_terminate', worker.terminate);
+			define(worker, 'terminate', terminate);
 
-		worker.terminate = function () {
-			this._terminate();
-			URL.revokeObjectURL(this._url);
-		};
+			if (inlineWorker.type) return worker;
 
-		inlineWorker.type = 'blob';
+			inlineWorker.type = typeName;
+			inlineWorker.createURL = type;
 
-		return worker;
+			return worker;
+		} catch (e) {
+			return null;
+		}
+	}
 
-	} catch (e) {}
+	var createDynURL = Sink.createDynURL;
+	var worker;
 
-	try {
-		worker			= new Worker('data:text/javascript;base64,' + btoa(script));
-		inlineWorker.type	= 'data';
+	if (inlineWorker.createURL) {
+		return wrap(inlineWorker.createURL, script, inlineWorker.type);
+	}
 
-		return worker;
+	worker = wrap(createDynURL.createBlob, script, 'blob');
+	if (worker) return worker;
 
-	} catch (e) {}
+	worker = wrap(createDynURL.createBlobBuilder, script, 'blobbuilder');
+	if (worker) return worker;
+
+	worker = wrap(createDynURL.createData, script, 'data');
 
 	return worker;
 }
 
-inlineWorker.ready = inlineWorker.working = false;
-
 Sink.EventEmitter.call(inlineWorker);
 
 inlineWorker.test = function () {
-	var	worker	= inlineWorker('this.onmessage=function (e){postMessage(e.data)}'),
-		data	= 'inlineWorker';
 	inlineWorker.ready = inlineWorker.working = false;
+	inlineWorker.type = '';
+	inlineWorker.createURL = null;
+
+	var worker = inlineWorker('this.onmessage=function(e){postMessage(e.data)}');
+	var data = 'inlineWorker';
 
 	function ready (success) {
 		if (inlineWorker.ready) return;
-		inlineWorker.ready	= true;
-		inlineWorker.working	= success;
+
+		inlineWorker.ready = true;
+		inlineWorker.working = success;
 		inlineWorker.emit('ready', [success]);
 		inlineWorker.off('ready');
 
@@ -73,12 +95,16 @@ inlineWorker.test = function () {
 	}
 
 	if (!worker) {
-		ready(false);
+		setTimeout(function () {
+			ready(false);
+		}, 0);
 	} else {
 		worker.onmessage = function (e) {
 			ready(e.data === data);
 		};
+
 		worker.postMessage(data);
+
 		setTimeout(function () {
 			ready(false);
 		}, 1000);
